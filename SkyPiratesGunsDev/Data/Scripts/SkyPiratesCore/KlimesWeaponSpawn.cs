171fadc527a0909c1cae70a14f29df2189abd42a
jphuffman <jonphuff@gmail.com>
2024-07-04 10:40:25 -0400

    sound block range increased, pistol added on spawn

---
+++ SkyPiratesGunsDev/Data/Scripts/SkyPiratesCore/KlimesWeaponSpawn.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Sandbox.Game.Entities;
+using Sandbox.Game;
+
+using Sandbox.ModAPI;
+using VRageMath;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Game.Components;
+using Sandbox.Definitions;
+//using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using ParallelTasks;
+using VRage.Game;
+using VRage.Game.ModAPI;
+using VRage.Game.ModAPI.Interfaces;
+using Sandbox.Game.Lights;
+
+using System.Threading;
+using System.Text;
+using VRage.Utils;
+using VRage.Library.Utils;
+using Sandbox.Game.SessionComponents;
+using Sandbox.Graphics;
+using VRage;
+using Sandbox.Game.Entities.Cube;
+using VRage.Game.Entity;
+
+
+namespace bob
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
+    public class gunAdd : MySessionComponentBase
+    {
+
+        public override void Init(MyObjectBuilder_SessionComponent sessionComponent)
+        {
+            if (MyAPIGateway.Session.IsServer)
+            {
+                //MyVisualScriptLogicProvider.PlayerSpawned += PlayerSpawned;
+            }
+        }
+
+        public override void BeforeStart()
+        {
+            if (MyAPIGateway.Session.IsServer)
+            {
+                MyVisualScriptLogicProvider.PlayerSpawned += PlayerSpawned;
+            }
+        }
+
+        private void PlayerSpawned(long playerId)
+        {
+            if (MyAPIGateway.Session.IsServer)
+            {
+                MyVisualScriptLogicProvider.ClearAllToolbarSlots(playerId);
+                MyDefinitionId reuse_def;
+
+              
+                if (MyDefinitionId.TryParse("MyObjectBuilder_PhysicalGunObject/SemiAutoPistolItem", out reuse_def))
+                {
+                    MyVisualScriptLogicProvider.AddToPlayersInventory(playerId, reuse_def, 1);
+                    MyVisualScriptLogicProvider.SetToolbarSlotToItem(0, reuse_def, playerId);
+                }
+                if (MyDefinitionId.TryParse("MyObjectBuilder_AmmoMagazine/SemiAutoPistolMagazine", out reuse_def))
+                {
+                    MyVisualScriptLogicProvider.AddToPlayersInventory(playerId, reuse_def, 3);
+                }
+            }
+        }
+
+        protected override void UnloadData()
+        {
+            if (MyAPIGateway.Session.IsServer)
+            {
+                MyVisualScriptLogicProvider.PlayerSpawned -= PlayerSpawned;
+            }
+        }
+    }
+}
