2fd48f150b8a4bad7d6590eaa4fbc119f70de928
Firewarrior44 <cairney.ryan@gmail.com>
2024-07-04 18:23:14 -0700

    Add script to remove AI
    - Add script to disable AI from all turret blocks

---
+++ SkyPiratesCoreDev/Data/Scripts/SkyPirates/WeaponAI.cs
@@ -0,0 +1,45 @@
+﻿using Sandbox.Definitions;
+using VRage.Game;
+using VRage.Game.Components;
+
+namespace cleaner
+{
+    /// <summary>
+    /// Handles removal of AI behaviour from all blocks of <see cref="MyLargeTurretBaseDefinition"/>
+    /// </summary>
+    [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation)]
+    public class Core : MySessionComponentBase
+    {
+        private static float AiRange = 0f;
+
+        private const bool AiEnabled = false;
+
+        /// <summary>
+        /// Removes Ai from turret blocks.
+        /// </summary>
+        private void RemoveAI()
+        {
+            foreach (MyDefinitionBase def in MyDefinitionManager.Static.GetAllDefinitions())
+            {
+                MyCubeBlockDefinition blockDef = def as MyLargeTurretBaseDefinition;
+                var turretDef = def as MyLargeTurretBaseDefinition;
+
+                if (blockDef == null)
+                {
+                    continue;
+                }
+
+                if (turretDef != null)
+                {
+                    turretDef.AiEnabled = Core.AiEnabled;
+                    turretDef.MaxRangeMeters = Core.AiRange;
+                }
+            }
+        }
+
+        public override void BeforeStart()
+        {
+            this.RemoveAI();
+        }
+    }
+}
