ec4bf9339c86531c2db87d7ecaa228df252eb6de
jphuffman <jonphuff@gmail.com>
2024-07-09 00:09:20 -0400

    hydro refiller added

---
+++ SkyPiratesCoreDev/Data/Scripts/SkyPirates/SuitHydroRefiller.cs
@@ -0,0 +1,62 @@
+using Sandbox.Game;
+using Sandbox.ModAPI;
+using System.Collections.Generic;
+using VRage.Game.Components;
+//using Sandbox.ModAPI.Ingame;
+using VRage.Game.ModAPI;
+
+namespace hydrorefiller
+{
+
+    [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation  )]
+    public class Bession : MySessionComponentBase
+    {
+        public double tock = 0;
+        public int delay = 30;
+        public static Dictionary<long, long> playerdictionary = new Dictionary<long, long>();
+
+        public override void UpdateBeforeSimulation()
+        {
+
+            tock++;
+            if (tock <= 60)
+            {
+                return;
+            }
+            tock = 0;
+
+            List<IMyPlayer> playerlist = new List<IMyPlayer>();
+            MyAPIGateway.Players.GetPlayers(playerlist);
+
+            foreach (var player in playerlist)
+            {
+                float hydro = MyVisualScriptLogicProvider.GetPlayersHydrogenLevel(player.Identity.IdentityId);
+
+                var UpdatedDictionary = new Dictionary<long, long>();
+                bool nohydro = hydro <= 0f;
+                // add to list if they are not in the list and they dotn have hydro
+                if (!playerdictionary.ContainsKey(player.Identity.IdentityId) && nohydro)
+                {
+                    UpdatedDictionary[player.Identity.IdentityId] = 0;
+                }
+                else if (playerdictionary.ContainsKey(player.Identity.IdentityId))
+                {
+
+                    var elapsedTime = playerdictionary[player.Identity.IdentityId] + 1;
+                    if (elapsedTime > delay)
+                    {
+                        MyVisualScriptLogicProvider.SetPlayersHydrogenLevel(player.Identity.IdentityId, 1f);
+
+                    }
+                    else
+                    {
+                        UpdatedDictionary[player.Identity.IdentityId] = elapsedTime;
+                    }
+                }
+
+                playerdictionary = UpdatedDictionary;
+            }
+
+        }
+    }
+}
