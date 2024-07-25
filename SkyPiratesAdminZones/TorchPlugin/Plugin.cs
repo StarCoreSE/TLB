eb99cf089f66fd232638afb75d163f3ad0be3f56
xianson <137644917+xianson@users.noreply.github.com>
2024-07-25 17:18:25 -0500

    woopsies

--- SkyPiratesAdminZones/TorchPlugin/Plugin.cs
+++ SkyPiratesAdminZones/TorchPlugin/Plugin.cs
@@ -2,25 +2,86 @@
 using System.Collections.Generic;
 using NLog;
 using Sandbox;
-using Sandbox.Game;
 using Sandbox.Game.World;
 using Sandbox.ModAPI;
 using Torch;
 using Torch.API;
+using VRage.Game.Components;
 using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
 
-namespace AutoPromoTorchFixed
+namespace BobAdminZone
 {
-    public class AutoPromoTorchFixed : TorchPluginBase
+    /*
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_EntityBase), false, "ADMIN_Zone_Beacon")]
+    public class BobAdminZoneBlock : MyGameLogicComponent
     {
+        IMyBeacon beacon;
+
+        BobAdminZoneBlock()
+        {
+            this.RegisterForEntityEvent()
+        }
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            beacon = Entity as IMyBeacon;
+            BobAdminZone.instance.beacons.Add(beacon);
+        }
+
+        public override void Close()
+        {
+            BobAdminZone.instance.beacons.Remove(beacon);
+        }
+    }
+    */
+
+    public class BobAdminZoneSession : MySessionComponentBase
+    {
+        public override void LoadData()
+        {
+            MyAPIGateway.Entities.OnEntityAdd += HandleEntityAdded;
+            MyAPIGateway.Entities.OnEntityRemove += HandleEntityRemoved;
+        }
+
+        private void HandleEntityAdded(IMyEntity entity)
+        {
+            if (entity != null && entity is IMyBeacon && (entity as IMyCubeBlock).SlimBlock.BlockDefinition.Id.SubtypeId.String.Contains("ADMIN"))
+            {
+                BobAdminZone.instance.beacons.Add(entity as IMyBeacon);
+            }
+        }
+
+        private void HandleEntityRemoved(IMyEntity entity)
+        {
+            if (entity != null && entity is IMyBeacon && (entity as IMyCubeBlock).SlimBlock.BlockDefinition.Id.SubtypeId.String.Contains("ADMIN"))
+            {
+                BobAdminZone.instance.beacons.Remove(entity as IMyBeacon);
+            }
+        }
+
+        protected override void UnloadData()
+        {
+            MyAPIGateway.Entities.OnEntityAdd -= HandleEntityAdded;
+            MyAPIGateway.Entities.OnEntityRemove -= HandleEntityRemoved;
+        }
+    }
+
+    public class BobAdminZone : TorchPluginBase
+    {
+        public static BobAdminZone instance;
+        public List<IMyBeacon> beacons = new List<IMyBeacon>();
+
         private DateTime lastPromotionTime = DateTime.MinValue;
         public static readonly Logger Log = LogManager.GetCurrentClassLogger();
         private List<IMyPlayer> all_players = new List<IMyPlayer>();
-        private List<IMyPlayer> promoted_players = new List<IMyPlayer>();
+        private List<IMyPlayer> temporary_admins = new List<IMyPlayer>();
 
         public override void Init(ITorchBase torch)
         {
             base.Init(torch);
+            instance = this;
             torch.GameStateChanged += new TorchGameStateChangedDel(this.Torch_GameStateChanged);
         }
 
@@ -30,7 +91,8 @@ namespace AutoPromoTorchFixed
             {
                 if (MyAPIGateway.Session != null && MyAPIGateway.Session.IsServer)
                 {
-                    // Initialize any loaded state-specific logic here
+                    BobAdminZoneSession sesh = new BobAdminZoneSession();
+                    MySession.Static.RegisterComponent(sesh, MyUpdateOrder.BeforeSimulation, 99);
                 }
             }
             else if (newState == TorchGameState.Unloading && MyAPIGateway.Session != null && MyAPIGateway.Session.IsServer)
@@ -45,14 +107,32 @@ namespace AutoPromoTorchFixed
             MyAPIGateway.Multiplayer.Players.GetPlayers(all_players, null);
             foreach (IMyPlayer myPlayer in all_players)
             {
-                if (myPlayer.PromoteLevel != MyPromoteLevel.SpaceMaster && myPlayer.PromoteLevel != MyPromoteLevel.Admin)
+                bool shouldPromote = false;
+                foreach (IMyBeacon beacon in beacons)
+                {
+                    double distanceSquared = (myPlayer.GetPosition() - beacon.WorldMatrix.Translation).LengthSquared();
+                    if (distanceSquared < beacon.Radius * beacon.Radius)
+                        shouldPromote = true;
+                }
+
+                if (shouldPromote && myPlayer.PromoteLevel != MyPromoteLevel.SpaceMaster && myPlayer.PromoteLevel != MyPromoteLevel.Admin)
                 {
                     MySession mySession = MyAPIGateway.Session as MySession;
-                    if (mySession != null && !promoted_players.Contains(myPlayer))
+                    if (mySession != null && !temporary_admins.Contains(myPlayer))
                     {
                         mySession.SetUserPromoteLevel(myPlayer.SteamUserId, MyPromoteLevel.Admin);
                         Log.Info($"Promoted {myPlayer.DisplayName} to {myPlayer.PromoteLevel}");
-                        promoted_players.Add(myPlayer);
+                        temporary_admins.Add(myPlayer);
+                    }
+                }
+                else if (!shouldPromote && myPlayer.PromoteLevel == MyPromoteLevel.SpaceMaster)
+                {
+                    MySession mySession = MyAPIGateway.Session as MySession;
+                    if (mySession != null && temporary_admins.Contains(myPlayer))
+                    {
+                        mySession.SetUserPromoteLevel(myPlayer.SteamUserId, MyPromoteLevel.None);
+                        Log.Info($"Demoted {myPlayer.DisplayName} to {myPlayer.PromoteLevel}");
+                        temporary_admins.Remove(myPlayer);
                     }
                 }
             }
