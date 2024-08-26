fdc251a23b5c69940308919cd70dabcf1b475ae8
wa la <spamman21234@gmail.com>
2024-08-26 19:41:23 +0100

    Jet engine stuff tweaking stuff stuff

--- ModernWarfareCore/Data/Scripts/ModernWarfareCore/MechanicalPower.cs
+++ ModernWarfareCore/Data/Scripts/ModernWarfareCore/MechanicalPower.cs
@@ -439,7 +439,6 @@ namespace MODERN_WARFARE_CORE
             if (planet == null)
                 planet = MyGamePruningStructure.GetClosestPlanet(grid.WorldMatrix.Translation);
 
-            thruster.PowerConsumptionMultiplier = 1 + (float)mach * 10f;
         }
 
         public override void UpdateBeforeSimulation()
@@ -449,6 +448,8 @@ namespace MODERN_WARFARE_CORE
             var speed = thruster.CubeGrid.Physics.LinearVelocity.Length();
             mach = speed / Utilities.SpeedOfSound(planet.GetAirDensity(grid.WorldMatrix.Translation));
 
+            thruster.PowerConsumptionMultiplier = thruster.MaxThrust / thruster.MaxEffectiveThrust;
+
             ApplyStallTorque();
             ApplyBadJetNoVtol();
         }
@@ -457,11 +458,11 @@ namespace MODERN_WARFARE_CORE
         {
             float inclination = Vector3.Dot(Vector3.Normalize(grid.Physics.Gravity), -thruster.WorldMatrix.Backward);
 
-            thruster.ThrustMultiplier = 1 + (float)mach * 2f;
+            thruster.ThrustMultiplier = 1 + (float)mach * 1f;
 
             if (inclination > .53)
                 thruster.ThrustMultiplier *= 2f - (.47f + inclination) * (.47f + inclination);
-
+            //MyAPIGateway.Utilities.ShowNotification($"{thruster.ThrustMultiplier}", 16);
         }
 
         private void ApplyStallTorque()
@@ -509,23 +510,42 @@ namespace MODERN_WARFARE_CORE
         double[] mrange = new double[] {0.0, 0.21428571, 0.42857143, 0.64285714, 0.85714286, 1.07142857, 1.28571429, 1.5, 1.71428571, 1.92857143, 2.14285714, 2.35714286, 2.57142857, 2.78571429, 3.0};
         double[] aoarange = new double[] {0.0, 0.02617994, 0.05235988, 0.07853982, 0.10471976, 0.13089969, 0.15707963, 0.18325957, 0.20943951, 0.23561945, 0.26179939};
 
+        MyResourceSourceComponent source;
+        float source_max_output = 69420f;
+
         public override void Init(MyObjectBuilder_EntityBase objectBuilder)
         {
             intake = Entity as IMyGasGenerator;
             grid = intake.CubeGrid;
+            source = intake.Components.Get<MyResourceSourceComponent>();
+            source_max_output = source.MaxOutput;
+
             this.NeedsUpdate |= MyEntityUpdateEnum.EACH_10TH_FRAME | MyEntityUpdateEnum.EACH_FRAME;
         }
-
-        public override void UpdateBeforeSimulation10()
+        /*
+        private float CalclateKeenInfluence(float airDensity, float MinPlanetaryInfluence = 0.3f, float MaxPlanetaryInfluence = 1f, float EffectivenessAtMinInfluence = 0f, float EffectivenessAtMaxInfluence = 1f)
         {
-            if (planet == null)
-                planet = MyGamePruningStructure.GetClosestPlanet(grid.WorldMatrix.Translation);
+            // CalculatePlanetaryInfluenceForceModKeen
+            float InvDiffMinMaxPlanetaryInfluence = 1f / (MaxPlanetaryInfluence - MinPlanetaryInfluence);
+            if (airDensity <= 0)
+            {
+                return EffectivenessAtMinInfluence;
+            }
+            else if (MaxPlanetaryInfluence != MinPlanetaryInfluence)
+            {
+                float value = (airDensity - MinPlanetaryInfluence) * InvDiffMinMaxPlanetaryInfluence;
 
-            intake.ProductionCapacityMultiplier = 1 + (float)mach * 10f;
-        }
+                return MathHelper.Lerp(EffectivenessAtMinInfluence, EffectivenessAtMaxInfluence, MathHelper.Clamp(value, 0f, 1f));
+            }
+            
+            return 0;
+        }*/
 
         public override void UpdateBeforeSimulation()
         {
+            if (planet == null && grid != null)
+                planet = MyGamePruningStructure.GetClosestPlanet(grid.WorldMatrix.Translation);
+
             if (planet == null || grid?.Physics == null) return;
 
             var speed = intake.CubeGrid.Physics.LinearVelocity.Length();
@@ -533,8 +553,6 @@ namespace MODERN_WARFARE_CORE
             mach = speed / Utilities.SpeedOfSound(planet.GetAirDensity(grid.WorldMatrix.Translation));
             vang = Utilities.VectorAngleBetween(intake.CubeGrid.Physics.LinearVelocity, intake.WorldMatrix.Forward);
             double cd = Utilities.BilinearInterpolation(cd_interp_table, mrange, aoarange, mach, vang);
-
-            //MyAPIGateway.Utilities.ShowNotification($"mach {mach:#.##}, vang {vang:#.##}, cd {cd:#.##}", 16);
 
             ApplyDrag(cd);
         }
